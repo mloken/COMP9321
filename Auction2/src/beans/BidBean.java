139290508d0c3521851ae2a5286648998294f99a
Marcus Løken <lm.loken@gmail.com>
2015-05-02 17:47:25 +1000

    Added Bean and DAO for Bids

---
+++ Auction2/src/beans/BidBean.java
@@ -0,0 +1,11 @@
+package beans;
+
+import java.util.Date;
+
+public class BidBean {
+	private int id;
+	private int itemId;
+	private int ownerId;
+	private float price;
+	private Date date;
+}
---
+++ Auction2/src/dao/support/BidDAOImpl.java
@@ -0,0 +1,7 @@
+package dao.support;
+
+import dao.BidDAO;
+
+public class BidDAOImpl implements BidDAO{
+	
+}
