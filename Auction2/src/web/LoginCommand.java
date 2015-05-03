88bd380c54fd2976647870216195df5e7975ca95
Marcus Løken <lm.loken@gmail.com>
2015-05-03 11:13:42 +1000

    Some changes to LoginCommand and added loginfafiled.jsp (used when UserLoginFailedException is caught, i.e. DataAccessException)

--- Auction2/src/web/LoginCommand.java
+++ Auction2/src/web/LoginCommand.java
@@ -5,14 +5,37 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class LoginCommand implements Command{
+import beans.UserBean;
+import business.UserLoginFailedException;
+import business.support.UserServiceImpl;
+
+public class LoginCommand implements Command {
+
+	private static UserServiceImpl userDao;
+
+	/** Creates a new instance of LoginCommand */
+	public LoginCommand() {
+		userDao = new UserServiceImpl();
+	}
 
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		try {
+			UserBean user = userDao.login(request.getParameter("username"),
+					request.getParameter("password"));
+			if (user == null) {
+				return "/login.jsp";
+			}
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
 
+			return "/homepage.jsp";
+		} catch (UserLoginFailedException e) {
+			e.printStackTrace();
+			return "/loginfailed.jsp";
+		}
+	}
 }
